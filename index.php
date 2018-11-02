f683ba1baf21fe336101fc3c20d0251ff7cc4630
Sleiman Tanios <sleiman@tanios.ca>
2018-11-02 03:13:52 -0400

    improved config

--- index.php
+++ index.php
@@ -6,7 +6,10 @@ require 'src/Addcontext.php';
 // Initialize the class
 use \TANIOS\Addcontext\Addcontext;
 
-$addcontext = new Addcontext( array("access_token"=>"ACCESS_TOKEN"));
+$addcontext = new Addcontext( array(
+	"access_token"=>"ACCESS_TOKEN",
+	"api_url"=>"API_URL"
+));
 
 
 // Add any metadata to an array 
@@ -15,14 +18,19 @@ $metadata['sent_from']   = 'iphone';
 $metadata['tweet_id'] = '123213213';
 $metadata['tweet_content']    = 'messsage text';
 
+$identity['email'] = "email"; //required
+$identity['first_name'] = "first_name"; //optional
+
+
 // Add all the information you have to an array (email, datetime and rule are mandatory)
 // Add the metada array to the fields array
 
 $fields = array(
-	'email' => "sleiman@tanios.ca",
 	'datetime'=> "2018-03-01 19:08:00",
     'rule' => 'trump tweeted',
-    'metadata'=> $metadata
+    'metadata'=> $metadata,
+    'identity'=>$identity
+
 );
 
 // Create the snapshot 
--- src/Addcontext.php
+++ src/Addcontext.php
@@ -19,15 +19,16 @@ namespace TANIOS\Addcontext;
 class Addcontext 
 {
     
-    const API_URL = "https://addcontext.io/api/";
-
     private $_key;
+
+    private $_url;
 
     
     public function __construct($config)
     {
         if (is_array($config)) {
             $this->setKey($config['access_token']);
+            $this->setUrl($config['api_url']);
         } else {
             echo 'Error: __construct() - Configuration data is missing.';
         }
@@ -43,11 +44,21 @@ class Addcontext
         return $this->_key;
     }
 
+    public function setUrl($url)
+    {
+        $this->_url = $url;
+    }
+
+    public function getUrl()
+    {
+        return $this->_url;
+    }
+
 
     function createSnapshotRequest($fields){
 
         $fields_string = "";
-        $url = 'https://addcontext.io/api/snapshot_requests';
+        $api_url = $this->getUrl()."/snapshot_requests";
 
     
     
@@ -58,16 +69,16 @@ class Addcontext
         ];
 
 
-        $post = $this->curlPost($url,$fields,$fields_string,$headers);
+        $post = $this->curlPost($api_url,$fields,$fields_string,$headers);
         return $post;
 
     }
 
-    function curlPost($url,$fields,$fields_string,$headers){
+    function curlPost($api_url,$fields,$fields_string,$headers){
 
         $ch = curl_init();
 
-        curl_setopt($ch,CURLOPT_URL, $url);
+        curl_setopt($ch,CURLOPT_URL, $api_url);
         curl_setopt($ch,CURLOPT_POST, count($fields));
         curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
